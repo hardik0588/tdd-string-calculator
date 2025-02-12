b0728b52a5cff2a73ffc6f4da4c3327739703f43
HARDIK\hardi <hardikmer3042001@gmail.com>
2025-02-12 09:18:22 +0530

    return 0 for an empty string

--- string_calcultor/lib/string_calcultor.dart
+++ string_calcultor/lib/string_calcultor.dart
@@ -1,3 +1,5 @@
-int calculate() {
-  return 6 * 7;
+class StringCalculator {
+  int add(String input) {
+    return 0;
+  }
 }
--- string_calcultor/test/string_calcultor_test.dart
+++ string_calcultor/test/string_calcultor_test.dart
@@ -2,7 +2,13 @@ import 'package:string_calcultor/string_calcultor.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  late StringCalculator calculator;
+
+  setUp(() {
+    calculator = StringCalculator();
+  });
+
+  test('returns 0 for an empty string', () {
+    expect(calculator.add(""), 0);
   });
 }

ac2d4c7a2fbf31c45233a60dd0789d4450e61966
HARDIK\hardi <hardikmer3042001@gmail.com>
2025-02-12 08:47:41 +0530

    setup testing framework for TDD

---
+++ string_calcultor/bin/string_calcultor.dart
@@ -0,0 +1,5 @@
+import 'package:string_calcultor/string_calcultor.dart' as string_calcultor;
+
+void main(List<String> arguments) {
+  print('Hello world: ${string_calcultor.calculate()}!');
+}
---
+++ string_calcultor/test/string_calcultor_test.dart
@@ -0,0 +1,8 @@
+import 'package:string_calcultor/string_calcultor.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

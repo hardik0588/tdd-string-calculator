6a8f2c731663ba0bb7f3fdca8f8032e635344bdf
HARDIK\hardi <hardikmer3042001@gmail.com>
2025-02-12 09:45:39 +0530

    test case: supports custom delimiters

--- string_calcultor/lib/string_calcultor.dart
+++ string_calcultor/lib/string_calcultor.dart
@@ -3,7 +3,21 @@ class StringCalculator {
     if (input.isEmpty) {
       return 0;
     }
-    return _sumOfNumbers(input.split(RegExp(r"[,\n]")));
+
+    List<String> numbers = _extractNumbers(input);
+    return _sumOfNumbers(numbers);
+  }
+
+  List<String> _extractNumbers(String input) {
+    String delimiterPattern = r"[,\n]";
+
+    if (input.startsWith("//")) {
+      var parts = input.split("\n");
+      delimiterPattern = RegExp.escape(parts[0].substring(2));
+      input = parts[1];
+    }
+
+    return input.split(RegExp(delimiterPattern));
   }
 
   int _sumOfNumbers(List<String> numbers) {
--- string_calcultor/test/string_calcultor_test.dart
+++ string_calcultor/test/string_calcultor_test.dart
@@ -23,4 +23,9 @@ void main() {
   test('newlines between numbers and some of that numbers', () {
     expect(calculator.add("1\n2,3"), 6);
   });
+
+  test('supports custom delimiters', () {
+    expect(calculator.add("//;\n1;2"), 3);
+  });
+
 }
